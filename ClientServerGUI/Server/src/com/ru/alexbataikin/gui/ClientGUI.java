64738285102d786c88e3a55f4f739a786a2fa699
poof-poof <lexa10.95@mail.ru>
2018-05-21 16:54:30 +0300

    create GUI Server

---
+++ ClientServerGUI/Server/src/com/ru/alexbataikin/gui/ClientGUI.java
@@ -0,0 +1,54 @@
+package com.ru.alexbataikin.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ClientGUI extends JFrame {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientGUI();
+            }
+        });
+    }
+
+    public static final int WIDTH = 500;
+    public static final int HEIGHT = 300;
+
+    private JButton btnStart = new JButton("Запустить сервер");
+    private JButton btnStop = new JButton("Остановить сервер");
+    private JTextArea log = new JTextArea();
+
+    ClientGUI(){
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(WIDTH,HEIGHT);
+        setLocationRelativeTo(null);
+        setTitle("Сервер");
+
+        JPanel control = new JPanel(new GridLayout(1,2));
+        control.add(btnStart);
+        control.add(btnStop);
+
+        JScrollPane scrol = new JScrollPane(log);
+        scrol.setAutoscrolls(true);
+        log.setLineWrap(true);
+        log.setEditable(false);
+        log.setCursor(Cursor.getPredefinedCursor(Cursor.TEXT_CURSOR));
+
+        add(scrol);
+        add(control,BorderLayout.SOUTH);
+
+        btnStart.addActionListener(e -> {
+            log.append("Server started!\n");
+        });
+        btnStop.addActionListener(e -> {
+            log.append("Server stop\n");
+        });
+
+        setResizable(false);
+        setVisible(true);
+    }
+
+}
